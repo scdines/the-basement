2437672f475fb67b9a3c076c76d19a34071576a3
Laith <info@laithazzam.com>
2015-03-09 22:53:33 -0700

    rework

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -10,12 +10,49 @@
 // remap jQuery to $
 (function($){
 
+		var dBtn = $('#splash h3');
+		var logo = $('#main h1');
 
 		function sizeGrids(){
-			$('#spaces>ul>li').height($('#spaces li').width());
-			$('#plans').height(window.innerHeight)
+			$('#vendors>ul>li').height($('#vendors li').width());
+			$('#marketplace').height(window.innerHeight)
 		};
 
+		function userScroll(){
+			$(window).bind('touchmove scroll', function(e) { 
+				tDis = $(document).scrollTop();
+
+				if(tDis >= ((window.innerHeight/4)*3) ){
+					dBtn.css('opacity',0);
+					logo.css('margin-top','9px');
+				}else{
+					dBtn.css('opacity',1);
+					logo.css('margin-top','-100px');
+				}
+			});
+		};
+
+		function userClicks(){
+			$('#splash h3').bind('click', function(){
+				$('html, body').animate({
+			        scrollTop: window.innerHeight
+			    }, 500, function(){
+			    });
+			});
+
+			$('#main ul li a').on('click', function(){
+				var click = $(this).attr('href');
+				$('html, body').animate({
+			        scrollTop: $(""+click+"").offset().top
+			    }, 850);
+			    return false;
+			});
+
+			$('#navBtn').bind('click', function(){
+				$(this).toggleClass('active');
+				$('#main nav').toggleClass('open');
+			});
+		};
 		
 
 	/* trigger when page is ready */
@@ -23,6 +60,8 @@
 	
 		// your functions go here
 		sizeGrids();
+		userScroll();
+		userClicks();
 	
 	});
 	
