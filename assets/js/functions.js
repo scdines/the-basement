7284ec0bdec722ecf51e5261ffbc0c53336e5abd
Laith <info@laithazzam.com>
2015-03-10 23:30:30 -0700

    lookin good

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -14,8 +14,10 @@
 		var logo = $('#main h1');
 
 		function sizeGrids(){
-			$('#vendors>ul>li').height($('#vendors li').width());
-			$('#marketplace').height(window.innerHeight)
+			setTimeout(function(){
+				$('#vendors>ul>li').height($('#vendors li').width());
+				$('#marketplace').height(window.innerHeight);
+			})
 		};
 
 		function userScroll(){
@@ -52,6 +54,72 @@
 				$(this).toggleClass('active');
 				$('#main nav').toggleClass('open');
 			});
+
+
+
+	    	var iframe = document.getElementById('video');
+			var player = $f(iframe);
+			$('#film h2').on('click',function(){
+		    	$('#videoOverlay').css({
+						display: 'inherit',
+						opacity: 0
+					});
+				$('#videoOverlay').animate({opacity:'1'}, function(){
+					player.api("play");
+				});
+		    });
+		    $('#videoOverlay .close').on('click', function(){
+		    	player.api("pause");
+				$('#videoOverlay').fadeOut();
+		    });
+
+		    $(document).keyup(function(e) {
+			  if (e.keyCode == 27) {
+			  	player.api("pause");
+				$('#videoOverlay').fadeOut();
+			  } 
+			});
+		};
+
+		function scrollNav(){
+			/**
+		     * This part handles the highlighting functionality.
+		     * We use the scroll functionality again, some array creation and 
+		     * manipulation, class adding and class removing, and conditional testing
+		     */
+		    var aChildren = $("nav li").children(); // find the a children of the list items
+		    var aArray = []; // create the empty aArray
+		    for (var i=0; i < aChildren.length; i++) {    
+		        var aChild = aChildren[i];
+		        var ahref = $(aChild).attr('href');
+		        console.log(ahref);
+		        aArray.push(ahref);
+		    } // this for loop fills the aArray with attribute href values
+
+		    $(window).scroll(function(){
+		        var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
+		        var windowHeight = $(window).height(); // get the height of the window
+		        var docHeight = $(document).height();
+
+		        for (var i=0; i < aArray.length; i++) {
+		            var theID = aArray[i];
+		            var divPos = $(theID).offset().top; // get the offset of the div from the top of page
+		            var divHeight = $(theID).height(); // get the height of the div in question
+		            if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
+		                $("a[href='" + theID + "']").addClass("nav-active");
+		            } else {
+		                $("a[href='" + theID + "']").removeClass("nav-active");
+		            }
+		        }
+
+		        if(windowPos + windowHeight == docHeight) {
+		            if (!$("nav li:last-child a").hasClass("nav-active")) {
+		                var navActiveCurrent = $(".nav-active").attr("href");
+		                $("a[href='" + navActiveCurrent + "']").removeClass("nav-active");
+		                $("nav li:last-child a").addClass("nav-active");
+		            }
+		        }
+		    });
 		};
 		
 
@@ -62,6 +130,7 @@
 		sizeGrids();
 		userScroll();
 		userClicks();
+		scrollNav();
 	
 	});
 	
